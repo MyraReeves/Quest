7ca28770521b26337fb2d946c88c9ce6f9621918
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-11-25 21:57:21 -0800

    Initial commit. Contains urls to several free API providers

---
+++ JS/API-list.js
@@ -0,0 +1,49 @@
+/*  HTTP Status Dogs:    https://http.dog/
+----------------------------------------------------------------------
+No Content dog = https://http.dog/204
+No Content cat = https://httpcats.com/204
+
+Request Denied dog = https://http.dog/999
+
+Not Found fish = https://http.fish/404
+Not Found goat = https://httpgoats.com/404
+*/
+
+
+
+/*  Deck of Cards:      https://www.deckofcardsapi.com/
+----------------------------------------------------------------------
+*/
+
+
+
+/*  NASA Open APIs:     https://api.nasa.gov/
+----------------------------------------------------------------------
+Astronomy Picture of the Day = GET https://api.nasa.gov/planetary/apod
+Example query:  https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
+*/
+
+
+
+/*  Weather APIs:
+----------------------------------------------------------------------
+OpenWeatherMap:   https://openweathermap.org/api
+Meteoblue weather:   https://www.meteoblue.com/en/user/login/index
+*/
+
+
+
+/*   Zen Quotes API:    https://zenquotes.io/
+----------------------------------------------------------------------
+Instructions:       https://docs.zenquotes.io/zenquotes-documentation/
+
+https://zenquotes.io/api/[mode]/[key]?option1=value&option2=value
+[mode] = Retrieval type [quotes, today, author, random]. REQUIRED.
+[key] = **OPTIONAL**  API key for use with premium subscriptions
+[options] = **OPTIONAL**  Additional options
+
+Instructions for How to Fetch Quotes for your Website or App ( https://zenquotes.io/api/quotes/[your_key] ):
+    "This is the most efficient way for developers to load random quotes. Use the standard [quotes] API call and store the results as a variable in your project. This will keep a cache of quotes that you can loop locally rather than calling the server each time you want to display a new quote.
+
+    After a set interval (an hour or so), call the ZenQuotes API again to refresh your local app cache. Using this method will keep load times down and free up resources for others as your user base increases. In the event the API service is unavailable, you will still have an available data set in your app to pull from."
+*/
