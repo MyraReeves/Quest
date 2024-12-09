45544897752678cb96a084755b0c45c2c8bae1dd
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-12-08 23:34:24 -0800

    Adds 4 'http status' animal fetch requests

--- JS/API-list.js
+++ JS/API-list.js
@@ -2,14 +2,51 @@
 ----------------------------------------------------------------------
 No Content dog = https://http.dog/204
 No Content cat = https://httpcats.com/204
+Found goat = https://httpgoats.com/302
 
 Request Denied dog = https://http.dog/999
 
+This is fine dog = https://http.dog/218
+
+Teapot cat = https://httpcats.com/418
+
 Not Found fish = https://http.fish/404
 Not Found goat = https://httpgoats.com/404
+
+Not Acceptable goat = https://httpgoats.com/406
 */
 
+const noContentCat = () => {
+    fetch(`https://httpcats.com/204.jpg`)
+    .then(imageResult => {
+        emptyImage = imageResult.url;
+        document.getElementById("noContent1").innerHTML = '<img src = "' + emptyImage + '"width = "40%">'
+    })
+}
 
+const noContentDog = () => {
+    fetch(`https://http.dog/204.jpg`)
+    .then(imageResult => {
+        emptyImage = imageResult.url;
+        document.getElementById("noContent2").innerHTML = '<img src = "' + emptyImage + '"width = "40%">'
+    })
+}
+
+const foundIt = () => {
+    fetch(`https://httpgoats.com/302.jpg`)
+    .then(jsonResult => {
+        deniedImage = jsonResult.url;
+        document.getElementById("foundIt").innerHTML = '<img src = "' + deniedImage + '"width="40%">'
+    })
+}
+
+const requestDenied = () => {
+    fetch(`https://http.dog/999.jpg`)
+    .then(jsonResult => {
+        deniedImage = jsonResult.url;
+        document.getElementById("denied").innerHTML = '<img src = "' + deniedImage + '"width="40%">'
+    })
+}
 
 /*  Deck of Cards:      https://www.deckofcardsapi.com/
 ----------------------------------------------------------------------
@@ -30,6 +67,36 @@ Example query:  https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
 OpenWeatherMap:   https://openweathermap.org/api
 Meteoblue weather:   https://www.meteoblue.com/en/user/login/index
 */
+const currentWeather = () => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=482fa0d7b1ec8b6fd90f126818b8698f`)
+    .then(response => response.json())
+    // document.getElementById("weather").innerHTML = "Cloudy with a chance of meatballs!  Dun dun duuuuunnnnn!";
+
+    // .catch(error => console.log(error));
+}
+
+
+
+// formEl.addEventListener('click', function(e) {
+//     e.preventDefault();
+  
+//     const year = yearEl.value;
+//     const month = monthEl.value;
+//     const date = dateEl.value;
+  
+//     // Fetch bestselling books for date and add top 5 to page:
+//   fetch(`https://api.nytimes.com/svc/books/v3/lists/${year}-${month}-${date}/hardcover-fiction/overview.json?api-key=${myKey}`)
+//   .then(response => response.json())
+//   .then(data => {const allBooks = data.results.books;
+//     let text = ""
+//     for (let i = 0; i < 5; i++) {
+//       text += "Title: " + allBooks[i].title + "<br> Author(s): " + allBooks[i].author + "<br> Description: " + allBooks[i].description + '<br> <img src="' + allBooks[i].book_image + '" alt="Image of the book cover" width="200" height="300"><br><br>';
+//     }
+//     document.getElementById("books-container").innerHTML = text;
+//   })
+//   .catch(error => console.log(error));
+  
+//   });
 
 
 
