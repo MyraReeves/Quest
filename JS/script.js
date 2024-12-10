060b581db2769114f9d9681636b5d9b225def958
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-12-10 02:57:29 -0800

    Adds Character class and constructs two character objects from it

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,3 @@
-
 // Homepage content:
 const landingPage = document.getElementById('game-content');
 const homepageBackground = document.createElement('div');
@@ -10,6 +9,8 @@ startButton.textContent = 'Start Game';
 startButton.setAttribute("id", "start");
 startButton.addEventListener('click', createCharacter)
 homepageBackground.appendChild(startButton)
+
+
 
 
 // Character creation page:
@@ -68,6 +69,7 @@ function createCharacter() {
     nameInput = document.createElement('input');
     nameInput.setAttribute('name', 'characterName');
     nameInput.setAttribute('type', 'text');
+    nameInput.setAttribute('id', 'nameElement');
     nameInput.setAttribute('required', 'true');
     fieldset.append(nameLabel, nameInput, document.createElement('br'));
 
@@ -79,7 +81,7 @@ function createCharacter() {
     maleInput.setAttribute('name', 'characterGender');
     maleInput.setAttribute('type', 'radio');
     maleInput.setAttribute('id', 'male');
-    maleInput.setAttribute('value', 'male');
+    maleInput.setAttribute('value', 'Male');
     const maleLabel = document.createElement('label');
     maleLabel.setAttribute('for', 'male');
     maleLabel.style.color = 'black';
@@ -88,7 +90,7 @@ function createCharacter() {
     femaleInput.setAttribute('name', 'characterGender');
     femaleInput.setAttribute('type', 'radio');
     femaleInput.setAttribute('id', 'female');
-    femaleInput.setAttribute('value', 'female');
+    femaleInput.setAttribute('value', 'Female');
     const femaleLabel = document.createElement('label');
     femaleLabel.setAttribute('for', 'female');
     femaleLabel.style.color = 'black';
@@ -97,9 +99,9 @@ function createCharacter() {
     nonBinaryInput.setAttribute('name', 'characterGender');
     nonBinaryInput.setAttribute('type', 'radio');
     nonBinaryInput.setAttribute('id', 'non-binary');
-    nonBinaryInput.setAttribute('value', 'non-binary');
+    nonBinaryInput.setAttribute('value', 'Non-binary');
     const nonBinaryLabel = document.createElement('label');
-    nonBinaryLabel.setAttribute('for', 'female');
+    nonBinaryLabel.setAttribute('for', 'non-binary');
     nonBinaryLabel.style.color = 'black';
     nonBinaryLabel.textContent = 'Non-binary';
     fieldset.append(maleInput, maleLabel, femaleInput, femaleLabel, nonBinaryInput, nonBinaryLabel, document.createElement('br'));
@@ -111,6 +113,7 @@ function createCharacter() {
     fieldset.appendChild(raceLabel);
     const selectRace = document.createElement('select');
     selectRace.setAttribute('name', 'characterRace');
+    selectRace.setAttribute('id', 'characterRace');
     selectRace.setAttribute('required', 'true');
     const chooseRace = document.createElement('option');
     chooseRace.setAttribute('value', '');
@@ -136,11 +139,161 @@ function createCharacter() {
     createCharacterButton.textContent = 'Create Character!';
     fieldset.append(document.createElement('br'), createCharacterButton);
 
-}
+    const theForm = document.getElementById('character-creation-form');
+    const nameElement = document.getElementById('nameElement');
+
+    theForm.addEventListener('submit', function(event) {
+        event.preventDefault();      
+        const chosenName = nameElement.value;
+        const chosenGender = document.querySelector('input[name=characterGender]:checked').value;
+        const chosenRace = document.querySelector('select[name=characterRace]').value;
+
+        enterBazaar(chosenName, chosenGender, chosenRace);
+    })
+};
+
+
+
+
+// //////////////////////////////////////////////////////////////////////////////////////////////////////
+// Assignment Requirement:  Use one or more Classes (must use static methods and/or prototype methods) //
+// /////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class Character {
-    constructor(name='Jane Doe', race='human') {
+    constructor(name='Jane Doe', race='human', gender='') {
         this.name = name;
         this.race = race;
+        this.gender = gender
     }
+};
+
+class Dwarf extends Character {
+    constructor(significance, name, race, gender){
+        super(name, race, gender);
+        this.size = 'small';
+        this.significance = significance;
+    }
+    logInstanceToConsole(){
+        console.log(`\nCharacter = ${this.significance} \n-------------------------------\nName: ${this.name} \nGender: ${this.gender} \nRace: ${this.race} \nStature: ${this.size}\n\n`)
+    }
+};
+class Elf extends Character {
+    constructor(significance, name, race, gender){
+        super(name, race, gender);
+        this.size = 'tall';
+        this.significance = significance;    
+    }
+    logInstanceToConsole(){
+        console.log(`\nCharacter = ${this.significance} \n-------------------------------\nName: ${this.name} \nGender: ${this.gender} \nRace: ${this.race} \nStature: ${this.size}\n\n`)
+    }
+};
+class Gnome extends Character {
+    constructor(significance, name, race, gender){
+        super(name, race, gender);
+        this.size = 'small';
+        this.significance = significance;    
+    }
+    logInstanceToConsole(){
+        console.log(`\nCharacter = ${this.significance} \n-------------------------------\nName: ${this.name} \nGender: ${this.gender} \nRace: ${this.race} \nStature: ${this.size}\n\n`)
+    }
+};
+class Halfling extends Character {
+    constructor(significance, name, race, gender){
+        super(name, race, gender);
+        this.size = 'small';
+        this.significance = significance;
+    }
+    logInstanceToConsole(){
+        console.log(`\nCharacter = ${this.significance} \n-------------------------------\nName: ${this.name} \nGender: ${this.gender} \nRace: ${this.race} \nStature: ${this.size}\n\n`)
+    }
+};
+class Human extends Character {
+    constructor(significance, name, race, gender){
+        super(name, race, gender);
+        this.size = 'tall';
+        this.significance = significance;
+    }
+    logInstanceToConsole(){
+        console.log(`\nCharacter = ${this.significance} \n-------------------------------\nName: ${this.name} \nGender: ${this.gender} \nRace: ${this.race} \nStature: ${this.size}\n\n`)
+    }
+};
+
+
+
+
+
+
+// //////////////////////////////////
+// Chapter 1:  The evening bazaar //
+// ////////////////////////////////
+const enterBazaar = (name, gender, race) => {
+    const chosenName = name;
+    const chosenGender = gender;
+    const chosenRace = race;
+
+    const baseCharacter = new Character(chosenName, chosenRace, chosenGender);
+    console.log("\nBase of character created by user:", baseCharacter, "\n\n");
+
+    let playerCharacter = '';
+    if (chosenRace === 'Dwarf'){
+        playerCharacter = new Dwarf('PLAYER CHARACTER', name, race, gender);
+    }
+    else if (chosenRace === 'Elf'){
+        playerCharacter = new Elf('PLAYER CHARACTER', name, race, gender);
+    }
+    else if (chosenRace === 'Gnome'){
+        playerCharacter = new Gnome('PLAYER CHARACTER', name, race, gender);
+    }
+    else if (chosenRace === 'Halfling'){
+        playerCharacter = new Halfling('PLAYER CHARACTER', name, race, gender);
+    }
+    else {
+        playerCharacter = new Human('PLAYER CHARACTER', name, race, gender);
+    };
+
+    playerCharacter.logInstanceToConsole();
+    // console.log(playerCharacter.name);
+
+    document.getElementById('introHeader').remove();
+    document.getElementById('create-character').remove();
+    const bazaarBackground = document.createElement('div');
+    bazaarBackground.classList.add('bazaar-background');
+    bazaarBackground.setAttribute("id", "bazaar");
+    const weatherWidget = '<div id="ww_cf581cbb2c2a8" v=\'1.3\' loc=\'id\' a=\'{"t":"horizontal","lang":"en","sl_lpl":1,"ids":["wl4025"],"font":"Arial","sl_ics":"one_a","sl_sot":"fahrenheit","cl_bkg":"image","cl_font":"#FFFFFF","cl_cloud":"#FFFFFF","cl_persp":"#81D4FA","cl_sun":"#FFC107","cl_moon":"#FFC107","cl_thund":"#FF5722","el_nme":3,"el_phw":3,"el_whr":3}\'><a href="https://weatherwidget.org/" id="ww_cf581cbb2c2a8_u" target="_blank">HTML Weather Widget for website</a></div><script async src="https://app3.weatherwidget.org/js/?id=ww_cf581cbb2c2a8"></script>';
+    bazaarBackground.innerHTML = weatherWidget;
+    landingPage.appendChild(bazaarBackground);
+
+    const textArea = document.createElement('div');
+    textArea.classList.add('textArea');
+    textArea.setAttribute('id', 'bazaarInteractions');
+    textArea.innerHTML = '<p>Brave ' + chosenName +', you have journeyed long, hard, and far from home on your quest. Shortly after sunset, you finally reach where the forested path gives way to a large open expanse outside the stone walls of Antigone.</p>    <img src="/Images/moonlit_forest_path_tiny.jpg" alt="Small image of a moonlit forest path just before an opening"><p>As you set up camp for the night, you begin hearing the sounds of many lively conversations coming from nearby. When you investigate, you find a bustling outdoor bazaar. A perfect opportunity to gather more information!</p>    <img src="/Images/pxfuelDOTcom_market-bazaar-people-crowd-night-evening_TINY.jpg" alt="Small image of a crowded, fire-lit, outdoor market in the early evening">    <div id="bazaarButtons">Choose whether to go to:<button type="button" class="choiceButton" onclick="">Food Vendors</button><button type="button" class="choiceButton" onclick="">Merchant Tents</button><button type="button" class="choiceButton" onclick="">Entertainment Zone</button></div>';
+    bazaarBackground.appendChild(textArea);
+
+
+    
+    /*
+    let magician = '';
+    let magicianName = 'Bordan the Magnificent';
+    let magicianRace = chosenRace;
+    let magicianGender = 'Male';
+
+    if (chosenRace === 'Dwarf'){
+        magician = new Dwarf('MAGICIAN', magicianName, magicianRace, magicianGender);
+    }
+    else if (chosenRace === 'Elf'){
+        magician = new Elf('MAGICIAN', magicianName, magicianRace, magicianGender);
+    }
+    else if (chosenRace === 'Gnome'){
+        magician = new Gnome('MAGICIAN', magicianName, magicianRace, magicianGender);
+    }
+    else if (chosenRace === 'Halfling'){
+        magician = new Halfling('MAGICIAN', magicianName, magicianRace, magicianGender);
+    }
+    else {
+        magician = new Human('MAGICIAN', magicianName, magicianRace, magicianGender);
+    };
+
+    magician.logInstanceToConsole();
+    */
+
 };
