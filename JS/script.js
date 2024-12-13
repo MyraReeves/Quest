eb2e3ceb2e69b91c5f5bc8ec02e712934c9d1b58
Myra <163362998+MyraReeves@users.noreply.github.com>
2024-12-12 17:22:54 -0800

    Adds the fortune teller's main vision function

--- JS/script.js
+++ JS/script.js
@@ -221,9 +221,9 @@ class Human extends Character {
 
 
 
-// //////////////////////////////////
-// Chapter 1:  The evening bazaar //
-// ////////////////////////////////
+                                // //////////////////////////////////
+                                // Chapter 1:  The evening bazaar //
+                                // ////////////////////////////////
 const enterBazaar = (name, gender, race) => {
     const chosenName = name;
     const chosenGender = gender;
@@ -264,18 +264,21 @@ const enterBazaar = (name, gender, race) => {
     landingPage.appendChild(bazaarBackground);
 
 
+    //////////////////////////////////////
+    //      INTRODUCTION TO BAZAAR     //
+    ////////////////////////////////////
     const textArea = document.createElement('div');
     textArea.classList.add('textArea');
     textArea.setAttribute('id', 'bazaarInteractions');
     textArea.innerHTML = '<p class="short-paragraph">Brave ' + chosenName +', you have journeyed long, hard, and far from home on your quest. Shortly after sunset, you finally reach where the forested path gives way to a large open expanse outside the stone walls of Antigone.</p>    <img src="./Images/moonlit_forest_path_tiny.jpg" alt="Small image of a moonlit forest path just before an opening"><p class="short-paragraph">As you set up camp for the night, you begin hearing the sounds of many lively conversations coming from nearby. When you investigate, you find a bustling outdoor bazaar. A perfect opportunity to gather more information!</p>    <img src="./Images/pxfuelDOTcom_market-bazaar-people-crowd-night-evening_TINY.jpg" alt="Small image of a crowded, fire-lit, outdoor market in the early evening">    <div id="bazaarButtons">Choose whether to go to:<button type="button" id="bazaarFood" class="choiceButton" onclick="">Food Vendors</button><button type="button" id="bazaarMerchants" class="choiceButton" onclick="">Merchant Tents</button><button type="button" id="bazaarEntertainment" class="choiceButton" onclick="">Entertainment Zone</button></div>';
     bazaarBackground.appendChild(textArea);
 
-
     const elementToErase = document.getElementById("weatherButton").nextSibling
-    // const removeBazaarChild = () => {
-    //     elementToErase.remove();
-    // };
 
+
+    ///////////////////////////
+    //      CENTRAL HUB     //
+    /////////////////////////
     function returnToEntrance() {
         let bazaarBackground = document.getElementById('bazaar');
         let indexOfElementToErase = (bazaarBackground.childNodes.length) - 1;
@@ -324,18 +327,72 @@ const enterBazaar = (name, gender, race) => {
         const merchantZone = document.createElement('div');
         merchantZone.classList.add('textArea');
         merchantZone.setAttribute("id", "merchant-zone");
-        merchantZone.innerHTML = '<p>Merchants fill the bulk of the outdoor market and encircle most of its perimeter, with some of the wealthiest vendors using semi-permanent structures directly against the outside of the city\'s walls to facilitate their trade. The area is alive with people shuffling hither and yon, and the sound of craftspeople hawking their wares rings through the brisk night air. Stalls offering every manner of goods litter the borders of the pathways, everything from beautiful jewelry to carpentry tools. From weaponry to painted vases, there is a sense that anything in the known world could be found in this market, if you have the coin to pay for it.<br><br></p>      <img src="./Images/merchants_tiny-from-pxfuelDOTcom.jpg" alt="Small image of various metal and fabric goods piled up against stone walls in an outdoor marketplace, with a path leading thru the middle">';
+        merchantZone.innerHTML = '<p>Merchants fill the bulk of the outdoor market and encircle most of its perimeter, with some of the wealthiest vendors using semi-permanent structures directly against the outside of the city\'s walls to facilitate their trade. The area is alive with people shuffling hither and yon, and the sound of craftspeople hawking their wares rings through the brisk night air. Stalls offering every manner of goods litter the borders of the pathways, everything from beautiful jewelry to carpentry tools. From weaponry to painted vases, there is a sense that anything in the known world could be found in this market, if you have the coin to pay for it.<br><br></p>      <img src="./Images/merchants_tiny-from-pxfuelDOTcom.jpg" alt="Small image of various metal and fabric goods piled up against stone walls in an outdoor marketplace, with a path leading thru the middle">    <p>A psychic with long flowing hair and a glittering black cloak calls out to you as you pass by her prophesies stall.  She bids you come have your fortune read.<br><br></p>    <div id="bazaarButtons"><button type="button" id="agnesNutter" class="choiceButton">Visit the Fortune Teller</button><button type="button" id="supplies" class="choiceButton">Buy Supplies</button><button type="button" id="otherStalls" class="choiceButton">Browse Small Merchandise Stalls</button></div>';
         bazaarBackground.appendChild(merchantZone);
+        document.getElementById('agnesNutter').addEventListener('click', fortuneTeller);
+        document.getElementById('supplies').addEventListener('click', buySupplies);
+        document.getElementById('otherStalls').addEventListener('click', smallStalls);
+    };
+    const bazaarMerchants = document.getElementById('bazaarMerchants');
+    bazaarMerchants.addEventListener('click', merchantVendors);
+
+
+    //////////////////////////////
+    //      FORTUNE TELLER     //
+    ////////////////////////////
+    const fortuneTeller = () => {
+        document.getElementById("weatherButton").nextSibling.remove();
 
         const fortuneTellerName = 'Agnes Nutter';
         const fortuneTellerRace = 'Human';
         const fortuneTellerGender = 'Female';
         const fortuneTeller = new Human('FORTUNE TELLER', fortuneTellerName, fortuneTellerRace, fortuneTellerGender);
         fortuneTeller.logInstanceToConsole();
-    };
-    const bazaarMerchants = document.getElementById('bazaarMerchants');
-    bazaarMerchants.addEventListener('click', merchantVendors);
 
+        fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+        // Note:  Due to uploading this to GitHub, I am refraining from using the personal/private API key that was assigned to me by NASA. NASA allows up to 30 requests per IP address per hour with the DEMO_KEY (up to 50 per day), which should be more than enough for this student class project.
+        .then(response => response.json())
+        .then(results => {
+            const dailyImage = results.url;
+            const altText = results.title;
+            const listenToAgnes = document.createElement('div');
+            listenToAgnes.setAttribute("id", "AstronomyPictureOfTheDay");
+            listenToAgnes.classList.add('result-of-choice-WIDER-VERSION');
+            listenToAgnes.innerHTML = '<p>You pay the fortune teller and sit down at her counter.</p><p>She smiles warmly at you and says, <i>"Hello, ' + playerCharacter.name + '.  I\'ve been waiting for you."</i> &nbsp; From beneath the velvet tablecloth draped over her counter, she removes a crystal ball and invites you to stare deep into the swirling, dark green clouds within it. &nbsp; <i>"Clear your mind and focus only on what is within,"</i> she instructs. . . </p><p>As you focus, the sounds of the bazaar fade from behind you and everything else around you drops away. . . &nbsp; . . . To your amazement the clouds within the ball suddenly flash a blinding, neon shade of purple and then slowly part! The following mysterious vision is revealed to you : <br></p><img src = "' + dailyImage + '" style = "border-radius: 50%; overflow: hidden; width: 400px; height: 400px; border: 2px solid black; display: block; margin: 0 auto;" alt="Image of ' + altText + '"><div id="bazaarButtons"><br><button type="button" id="ponder" class="choiceButton">Ponder the meaning of this vision</button><br><br><button type="button" id="supplies" class="choiceButton">Thank her and then <br>leave to go buy supplies</button><button type="button" id="otherStalls" class="choiceButton">What a waste of time!<br>Hurry off to the small merchandise stalls</button><button type="button" id="backToEntrance" class="choiceButton">Leave merchant area</button></div>'
+            bazaarBackground.appendChild(listenToAgnes);
+
+            document.getElementById('ponder').addEventListener('click', ponderTheCosmos);
+            document.getElementById('supplies').addEventListener('click', buySupplies);
+            document.getElementById('otherStalls').addEventListener('click', smallStalls);
+            document.getElementById('backToEntrance').addEventListener('click', returnToEntrance);
+
+        })
+        .catch(error => console.log(error));
+    };
+
+    const ponderTheCosmos = () => {
+        document.getElementById("weatherButton").nextSibling.remove();
+        // TO DO
+        // What a Nutter! <br>
+    };
+
+
+
+    ////////////////////////////
+    //      BUY SUPPLIES     //
+    //////////////////////////
+    const buySupplies = () => {
+        document.getElementById("weatherButton").nextSibling.remove();
+    };
+
+
+    ////////////////////////////
+    //      SMALL STALLS     //
+    //////////////////////////
+    const smallStalls = () => {
+        document.getElementById("weatherButton").nextSibling.remove();
+        // Chat & Listen In On Others
+    };
 
 
     /* =============================================================================================
@@ -385,9 +442,10 @@ const enterBazaar = (name, gender, race) => {
                 const openTent = document.createElement('div');
                 openTent.setAttribute("id", "magicianTent");
                 openTent.classList.add('result-of-choice');
-                openTent.innerHTML = '<img src = "' + deniedImage + '" style = "margin: 0 30%; width: 40%;"><p>As you reach out your hand to pull back the entrance flap, an angry voice barks, <i>"Can\'t you see the sign!?!"</i></p><p>A muscular dwarf dressed in armor labeled \'Security\' on the chest grabs your wrist and steps in front of the entrance.  Stabbing a stubby finger towards a small hanging sign on the side of the tent, she continues, <i>"Bordan the Magnificent is currently on his break in between performances!  Check back again later if you want to see his show!"</i></p><p>Judging by her angry glare, it is probably best to follow her advice instead of making a scene.</p><br><br>  <div id="bazaarButtons"><button type="button" id="fightDwarf" class="choiceButton">Argue with her anyways.<br>No one tells YOU what to do!</button><button type="button" id="shellGame" class="choiceButton">Visit gaming area</button><button type="button" id="watchShow" class="choiceButton">Attend a show</button><button type="button" id="backToEntrance" class="choiceButton" onclick = "returnToEntrance()">Leave entertainment area</button></div>'
+                openTent.innerHTML = '<img src = "' + deniedImage + '" style = "margin: 0 30%; width: 40%;"><p>As you reach out your hand to pull back the entrance flap, an angry voice barks, <i>"Can\'t you see the sign!?!"</i></p><p>A muscular dwarf dressed in armor labeled \'Security\' on the chest grabs your wrist and steps in front of the entrance.  Stabbing a stubby finger towards a small hanging sign on the side of the tent, she continues, <i>"Bordan the Magnificent is currently on his break in between performances!  Check back again later if you want to see his show!"</i></p><p>Judging by her angry glare, it is probably best to follow her advice instead of making a scene.</p><br><br>  <div id="bazaarButtons"><button type="button" id="fight" class="choiceButton">Argue with her anyways.<br>No one tells YOU what to do!</button><button type="button" id="shellGame" class="choiceButton">Visit gaming area</button><button type="button" id="watchShow" class="choiceButton">Attend a show</button><button type="button" id="backToEntrance" class="choiceButton">Leave entertainment area</button></div>'
                 bazaarBackground.appendChild(openTent);
 
+                document.getElementById('fight').addEventListener('click', fightDwarf);
                 document.getElementById('watchShow').addEventListener('click', attendAShow);
                 document.getElementById('shellGame').addEventListener('click', introToShellGame);
                 document.getElementById('backToEntrance').addEventListener('click', returnToEntrance);
@@ -399,6 +457,9 @@ const enterBazaar = (name, gender, race) => {
         // const goBackToEntrance = document.getElementById('backToEntrance');
         // goBackToEntrance.addEventListener('click', returnToEntrance);
 
+        const fightDwarf = () => {
+
+        };
 
 
         ///////////////////////////
